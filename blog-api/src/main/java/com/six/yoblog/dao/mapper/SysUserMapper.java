45a9ff3e177fab97639491ba3cb45a8274302498
Hollow <66857041+xisqiu@users.noreply.github.com>
2022-05-18 11:17:49 +0800

    Delete blog-api directory

--- blog-api/src/main/java/com/six/yoblog/dao/mapper/SysUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.six.yoblog.dao.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.six.yoblog.dao.pojo.SysUser;
-
-public interface SysUserMapper extends BaseMapper<SysUser> {
-}
